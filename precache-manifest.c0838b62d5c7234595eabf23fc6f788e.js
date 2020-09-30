2bb7e1d4deeca1c283f68f48b62502152e37a507
hidayatul-rk <71365495+hidayatul-rk@users.noreply.github.com>
2020-09-30 14:47:01 +0700

    Add files via upload

---
+++ precache-manifest.c0838b62d5c7234595eabf23fc6f788e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0acb5980c1bc33e3c689be9299306db7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13348d4ef1a4b758c8cb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4666ecc68d94bfcf0a51",
+    "url": "/static/js/2.d91129ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d91129ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13348d4ef1a4b758c8cb",
+    "url": "/static/js/main.4258ddb3.chunk.js"
+  },
+  {
+    "revision": "cca09dadcaf16f04d103",
+    "url": "/static/js/runtime-main.3b7f8721.js"
+  }
+]);
